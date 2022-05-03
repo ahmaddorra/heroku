07a71ba4e5cca541de610b4726849b99053068c2
ahmaddorra <ahmadeldorra1@gmail.com>
2022-05-04 02:11:35 +0300

    a

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0',debug=True)
+  app.run(host='0.0.0.0',debug=True, port=environ.get("PORT", 5000))
 
 @app.route("/v1/translate", methods=["GET", "POST"])
 def predict():
